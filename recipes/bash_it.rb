15b574a36f84d7113b47a9f89f803b772d86a3c3
Kaleb Walton <kalebwalton@gmail.com>
2015-06-22 15:09:07 -0400

    fix

--- recipes/bash_it.rb
+++ recipes/bash_it.rb
@@ -2,8 +2,7 @@ bash_it_version = version_string_for('bash_it')
 
 git "#{Chef::Config[:file_cache_path]}/bash_it" do
   repository node['bash_it']['repository']
-  revision bash_it_version
-  checkout_branch 'master'
+  revision master
   destination "#{Chef::Config[:file_cache_path]}/bash_it"
   action :sync
 end
